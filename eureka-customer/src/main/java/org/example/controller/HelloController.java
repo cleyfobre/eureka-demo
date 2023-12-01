637deb4a03018091e9bbb779c9862f55a5c8f1fd
Young <cleyfobre@gmail.com>
2023-12-01 15:19:24 +0900

    Add fraud service

--- eureka-customer/src/main/java/org/example/controller/HelloController.java
+++ eureka-customer/src/main/java/org/example/controller/HelloController.java
@@ -1,14 +1,22 @@
 package org.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("api/hello")
-    public String hello() {
-        return "hello";
+    public ResponseEntity<String> hello() {
+        String fooResourceUrl = "http://FRAUD/api/hello";
+        ResponseEntity<String> response = restTemplate.getForEntity(fooResourceUrl, String.class);
+        return response;
     }
 
 }
